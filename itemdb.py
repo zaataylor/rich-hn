775ea82781ae39459240662af07181b616a4201c
Zachary A. Taylor <ztaylor45685@gmail.com>
2021-01-17 14:08:32 -0500

    Added implementation outline for update_item method

--- itemdb.py
+++ itemdb.py
@@ -14,4 +14,17 @@ class ItemDB(object):
             self.items[i.get_id()] = i
 
     def update_item(self, i: Item):
+        # Algorithm:
+        #
+        # 1. Use the ID of the passed in Item to locate the desired Item
+        # in the items dictionary
+        #
+        # 2. Loop over the content dictionary of the passed-in Item, and
+        # for each key-value pair, update the existing Item's content
+        # dictionary with that data. For dictionary members of content
+        # like 'kids', make sure you don't overwrite the dictionary, but
+        # instead update it with the items in that dictionary. A lot of
+        # the other, non-dictionary fields in content should be overwritten
+        # since they might correspond to quantitative things -- like number of
+        # comments or number of points -- which need to be frequently updated.
         pass
